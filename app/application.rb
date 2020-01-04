b4cad11fe56ca8a923dd0aeb8b2418eca3554fab
rmiri <re_miriuk@hotmail.com>
2020-01-04 23:40:52 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+
+class Application
+
+    def call(env)
+      resp = Rack::Response.new
+
+
+        resp.write Time.now.hour < 12 ? 'morning' : 'afternoon'
+   
+   
+      resp.finish
+    end
+  
+  end
